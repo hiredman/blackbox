064ba7c976b10ab756e67cedb21c43333b2e2374
Kevin Downey <redchin@gmail.com>
2012-10-13 19:59:32 -0700

    whitespace

--- src/blackbox/gpio.clj
+++ src/blackbox/gpio.clj
@@ -37,46 +37,46 @@
 (def gpio-set-data-out 0x194)
 
 (def gpio
-  {"USR0"  [gpio1, (bit-shift-left 1 21),           "gpmc_a5"],
-   "USR1"  [gpio1, (bit-shift-left 1 22),           "gpmc_a6"],
-   "USR2"  [gpio1, (bit-shift-left 1 23),           "gpmc_a7"],
-   "USR3"  [gpio1, (bit-shift-left 1 24),           "gpmc_a8"],
-   "GPIO0_7"  [gpio0,  (bit-shift-left 1 7), "ecap0_in_pwm0_out"],
-   "GPIO0_26"  [gpio0, (bit-shift-left 1 26),         "gpmc_ad10"],
-   "GPIO0_27"  [gpio0, (bit-shift-left 1 27),         "gpmc_ad11"],
-   "GPIO1_0"  [gpio1,     1,          "gpmc_ad0"],
-   "GPIO1_1"  [gpio1,  (bit-shift-left 1 1),          "gpmc_ad1"],
-   "GPIO1_2"  [gpio1,  (bit-shift-left 1 2),          "gpmc_ad2"],
-   "GPIO1_3"  [gpio1,  (bit-shift-left 1 3),          "gpmc_ad3"],
-   "GPIO1_4"  [gpio1,  (bit-shift-left 1 4),          "gpmc_ad4"],
-   "GPIO1_5"  [gpio1,  (bit-shift-left 1 5),          "gpmc_ad5"],
-   "GPIO1_6"  [gpio1,  (bit-shift-left 1 6),          "gpmc_ad6"],
-   "GPIO1_7"  [gpio1,  (bit-shift-left 1 7),          "gpmc_ad7"],
-   "GPIO1_12"  [gpio1, (bit-shift-left 1 12),         "gpmc_ad12"],
-   "GPIO1_13"  [gpio1, (bit-shift-left 1 13),         "gpmc_ad13"],
-   "GPIO1_14"  [gpio1, (bit-shift-left 1 14),         "gpmc_ad14"],
-   "GPIO1_15"  [gpio1, (bit-shift-left 1 15),         "gpmc_ad15"],
-   "GPIO1_16"  [gpio1, (bit-shift-left 1 16),           "gpmc_a0"],
-   "GPIO1_17"  [gpio1, (bit-shift-left 1 17),           "gpmc_a1"],
-   "GPIO1_28"  [gpio1, (bit-shift-left 1 28),         "gpmc_ben1"],
-   "GPIO1_29"  [gpio1, (bit-shift-left 1 29),         "gpmc_csn0"],
-   "GPIO1_30"  [gpio1, (bit-shift-left 1 30),         "gpmc_csn1"],
-   "GPIO1_31"  [gpio1, (bit-shift-left 1 31),         "gpmc_csn2"],
-   "GPIO2_1"  [gpio2,  (bit-shift-left 1 1),          "gpmc_clk"],
-   "GPIO2_6"  [gpio2,  (bit-shift-left 1 6),         "lcd_data0"],
-   "GPIO2_7"  [gpio2,  (bit-shift-left 1 7),         "lcd_data1"],
-   "GPIO2_8"  [gpio2,  (bit-shift-left 1 8),         "lcd_data2"],
-   "GPIO2_9"  [gpio2,  (bit-shift-left 1 9),         "lcd_data3"],
-   "GPIO2_10"  [gpio2, (bit-shift-left 1 10),         "lcd_data4"],
-   "GPIO2_11"  [gpio2, (bit-shift-left 1 11),         "lcd_data5"],
-   "GPIO2_12"  [gpio2, (bit-shift-left 1 12),         "lcd_data6"],
-   "GPIO2_13"  [gpio2, (bit-shift-left 1 13),         "lcd_data7"],
-   "GPIO2_22"  [gpio2, (bit-shift-left 1 22),         "lcd_vsync"],
-   "GPIO2_23"  [gpio2, (bit-shift-left 1 23),         "lcd_hsync"],
-   "GPIO2_24"  [gpio2, (bit-shift-left 1 24),          "lcd_pclk"],
-   "GPIO2_25"  [gpio2, (bit-shift-left 1 25),    "lcd_ac_bias_en"],
-   "GPIO3_19"  [gpio3, (bit-shift-left 1 19),        "mcasp0_fsr"],
-   "GPIO3_21"  [gpio3, (bit-shift-left 1 21),     "mcasp0_ahclkx"]})
+  {"USR0" [gpio1, (bit-shift-left 1 21), "gpmc_a5"],
+   "USR1" [gpio1, (bit-shift-left 1 22), "gpmc_a6"],
+   "USR2" [gpio1, (bit-shift-left 1 23), "gpmc_a7"],
+   "USR3" [gpio1, (bit-shift-left 1 24), "gpmc_a8"],
+   "GPIO0_7" [gpio0,  (bit-shift-left 1 7), "ecap0_in_pwm0_out"],
+   "GPIO0_26" [gpio0, (bit-shift-left 1 26), "gpmc_ad10"],
+   "GPIO0_27" [gpio0, (bit-shift-left 1 27), "gpmc_ad11"],
+   "GPIO1_0" [gpio1, 1, "gpmc_ad0"],
+   "GPIO1_1" [gpio1, (bit-shift-left 1 1), "gpmc_ad1"],
+   "GPIO1_2" [gpio1,  (bit-shift-left 1 2), "gpmc_ad2"],
+   "GPIO1_3" [gpio1,  (bit-shift-left 1 3), "gpmc_ad3"],
+   "GPIO1_4" [gpio1,  (bit-shift-left 1 4), "gpmc_ad4"],
+   "GPIO1_5" [gpio1,  (bit-shift-left 1 5), "gpmc_ad5"],
+   "GPIO1_6" [gpio1,  (bit-shift-left 1 6), "gpmc_ad6"],
+   "GPIO1_7" [gpio1,  (bit-shift-left 1 7), "gpmc_ad7"],
+   "GPIO1_12" [gpio1, (bit-shift-left 1 12), "gpmc_ad12"],
+   "GPIO1_13" [gpio1, (bit-shift-left 1 13), "gpmc_ad13"],
+   "GPIO1_14" [gpio1, (bit-shift-left 1 14), "gpmc_ad14"],
+   "GPIO1_15" [gpio1, (bit-shift-left 1 15), "gpmc_ad15"],
+   "GPIO1_16" [gpio1, (bit-shift-left 1 16), "gpmc_a0"],
+   "GPIO1_17" [gpio1, (bit-shift-left 1 17), "gpmc_a1"],
+   "GPIO1_28" [gpio1, (bit-shift-left 1 28), "gpmc_ben1"],
+   "GPIO1_29" [gpio1, (bit-shift-left 1 29), "gpmc_csn0"],
+   "GPIO1_30" [gpio1, (bit-shift-left 1 30), "gpmc_csn1"],
+   "GPIO1_31" [gpio1, (bit-shift-left 1 31), "gpmc_csn2"],
+   "GPIO2_1" [gpio2, (bit-shift-left 1 1), "gpmc_clk"],
+   "GPIO2_6" [gpio2, (bit-shift-left 1 6), "lcd_data0"],
+   "GPIO2_7" [gpio2, (bit-shift-left 1 7), "lcd_data1"],
+   "GPIO2_8" [gpio2, (bit-shift-left 1 8), "lcd_data2"],
+   "GPIO2_9" [gpio2, (bit-shift-left 1 9), "lcd_data3"],
+   "GPIO2_10" [gpio2, (bit-shift-left 1 10), "lcd_data4"],
+   "GPIO2_11" [gpio2, (bit-shift-left 1 11), "lcd_data5"],
+   "GPIO2_12" [gpio2, (bit-shift-left 1 12), "lcd_data6"],
+   "GPIO2_13" [gpio2, (bit-shift-left 1 13), "lcd_data7"],
+   "GPIO2_22" [gpio2, (bit-shift-left 1 22), "lcd_vsync"],
+   "GPIO2_23" [gpio2, (bit-shift-left 1 23), "lcd_hsync"],
+   "GPIO2_24" [gpio2, (bit-shift-left 1 24), "lcd_pclk"],
+   "GPIO2_25" [gpio2, (bit-shift-left 1 25), "lcd_ac_bias_en"],
+   "GPIO3_19" [gpio3, (bit-shift-left 1 19), "mcasp0_fsr"],
+   "GPIO3_21" [gpio3, (bit-shift-left 1 21), "mcasp0_ahclkx"]})
 
 (def us
   (.get (doto (.getDeclaredField sun.misc.Unsafe "theUnsafe")
@@ -172,51 +172,49 @@
   (low! [_])
   (flip! [_]))
 
-(defprotocol SPTT
-  (throw1 [_])
-  (throw2 [_])
-  (throw3 [_]))
-
 (deftype Pin [pin
-              ^:volatile-mutable direction
+              ^:volatile-mutable dir
               ^:volatile-mutable state
               ^:volatile-mutable pull]
   clojure.lang.IDeref
   (deref [_]
-    (if (= :in direction)
+    (if (= :in dir)
       (digital-read pin)
       state))
   IMux
   (direction [_]
-    direction)
+    dir)
   (mux! [_ d p]
-    (set! direction d)
+    (set! dir d)
     (set! pull p)
-    (pin-mode pin direction pull))
+    (pin-mode pin dir pull))
   IFlipFlop
   (high! [_]
-    (assert (= direction :out))
+    (assert (= dir :out))
     (set! state true)
     (digital-write pin state))
   (low! [_]
-    (assert (= direction :out))
+    (assert (= dir :out))
     (set! state false)
     (digital-write pin state))
   (flip! [_]
-    (assert (= direction :out))
+    (assert (= dir :out))
     (set! state (not state))
     (digital-write pin state))
   Object
   (toString [this]
     (format "#<%s %s>"
             pin
-            (pr-str {:direction direction
+            (pr-str {:direction dir
                      :state state
                      :pull pull}))))
 
 (doseq [[pin & _] gpio]
   (intern *ns*
           (symbol (.toLowerCase pin))
+          (Pin. pin :out false nil))
+  (intern *ns*
+          (symbol pin)
           (Pin. pin :out false nil)))
 
 (defmethod print-method Pin [p writer]
