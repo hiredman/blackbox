2c4fc6cf1607cfb0f4eed1244763aca4861f085a
Kevin Downey <redchin@gmail.com>
2012-10-28 19:39:49 -0700

    watchdog the image loop

--- cljs/boot.cljs
+++ cljs/boot.cljs
@@ -16,15 +16,29 @@
     (.setAttribute img "src" src)
     img))
 
+
+(def check (atom 0))
+
+(defn g [context]
+  ((fn f []
+     (image (str "/snap.jpg?" (.random js/Math))
+            (fn [img]
+              (js/setTimeout f 50)
+              (.drawImage context img 0 0)
+              (reset! check 0))))))
+
 (defn ^:export main []
   (let [can (.getElementById js/document "snap")
         context (.getContext can "2d")]
     (.scale context scale-factor scale-factor)
-    ((fn f []
-       (image (str "/snap.jpg?" (.random js/Math))
-              (fn [img]
-                (js/setTimeout f 50)
-                (.drawImage context img 0 0)))))))
+    (g context)
+    (js/setInterval
+     (fn []
+       (swap! check inc)
+       (when (> @check 3)
+         (reset! check 0)
+         (g context)))
+     1000)))
 
 (defn ^:export led-flip []
   (ping "pin/gpio2_9"))
