ed106ab99d7c0fa4b30306564f6e1d81fa3f6c27
Kevin Downey <redchin@gmail.com>
2012-11-16 09:57:48 -0500

    update handlers to actually work

--- src/blackbox/web.clj
+++ src/blackbox/web.clj
@@ -10,22 +10,32 @@
   (while (nil? @cam/picture-file))
   @cam/picture-file)
 
+(defn mjpeg-handler [_]
+  {:status 200
+   :headers {"content-type" "multipart/x-mixed-replace; boundary=--spionisto"
+             "Cache-Control" "no-cache"}
+   :body (cam/mjpeg-stream)})
+
+(defn root-handler [_]
+  {:status 404
+   :body
+   (html
+    [:html
+     [:head]
+     [:body
+      [:div {:style "float:left;"}
+       [:div {:style "width:1em;height:1em;background-color:red;margin:1px;"}]
+       [:div {:style "width:1em;height:1em;background-color:black;margin:1px;"}]
+       [:div {:style "width:1em;height:1em;background-color:black;margin:1px;"}]
+       [:div {:style "width:1em;height:1em;background-color:black;margin:1px;"}]
+       ]
+      [:center
+       [:img {:src "/snap.jpg"}]
+       [:br]
+       ]]])})
+
 (defroutes handler*
-  (GET "/" []
-       {:status 404
-        :body (html
-               [:html
-                [:head]
-                [:body
-                 [:div {:style "float:left;"}
-                   [:div {:style "width:1em;height:1em;background-color:red;margin:1px;"}]
-                   [:div {:style "width:1em;height:1em;background-color:black;margin:1px;"}]
-                   [:div {:style "width:1em;height:1em;background-color:black;margin:1px;"}]
-                   [:div {:style "width:1em;height:1em;background-color:black;margin:1px;"}]]
-                 [:center
-                  [:img {:src "/snap.jpg"}]
-                  [:br]
-                  ]]])})
+  (GET "/" [] root-handler)
   (GET "/snap.jpg" []
        {:headers {"content-type" "image/jpeg"}
         :body (view-port)})
@@ -34,11 +44,7 @@
         :headers {"content-type" "video/ogg"
                   "Cache-Control" "no-cache"}
         :body (cam/video-stream)})
-  (GET "/mjpeg" []
-       {:status 200
-        :headers {"content-type" "multipart/x-mixed-replace; boundary=--spionisto"
-                  "Cache-Control" "no-cache"}
-        :body (cam/mjpeg-stream)})
+  (GET "/mjpeg" [] mjpeg-handler)
   (GET "/pin/:pin" [pin]
        (require 'blackbox.gpio)
        ((resolve 'blackbox.gpio/flip!)
@@ -58,16 +64,11 @@
        {:status 200})
   (route/not-found "<h1>Page not found</h1>"))
 
-
-
 (def handler (-> handler*
                  (wrap-resource "")))
-
 
 (comment
   (require 'blackbox.gpio)
   (blackbox.gpio/flip! blackbox.gpio/gpio2_9)
   (blackbox.gpio/mux! blackbox.gpio/gpio2_9 :out -1)
   )
-
-
